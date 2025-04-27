e8422ed949eb18185af4e26f7b18755309175bf8
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-04-26 23:22:30 -0500

    add EventModel interface to define event structure

---
+++ src/models/event.model.ts
@@ -0,0 +1,14 @@
+export interface EventModel {
+    id: string;
+    title: string;
+    description: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+    location: string;
+    imageUrl: string;
+    capacity: number;
+    spotsLeft: number;
+    categories: string[];
+  }
+  
