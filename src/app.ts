d80e2334762b4349d81ab4ab7d770461eac3d5a3
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-04-29 22:10:55 -0500

    add favorite routes to the main application file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { connectDB } from './database/mongo'; // 👈 Importar conexión
+import { connectDB } from './database/mongo';
 
 import eventRoutes from './routes/event.routes';
+import favoriteRoutes from './routes/favorite.routes';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/events', eventRoutes);
+app.use('/api/events', favoriteRoutes);   
 
 app.get('/', (req, res) => {
   res.send('Conference API running with MongoDB 🚀');
